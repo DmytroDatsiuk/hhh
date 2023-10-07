2defde06b0855b65c3b2a7787642d0bc443637c7
DmytroDatsiuk <darkside77789@gmail.com>
2023-10-07 02:28:17 +0000

    Deploying to gh-pages from @ DmytroDatsiuk/hhh@5e9e5b8c5d80df8ba618da41e76ff457d9f1b143 🚀

---
+++ 02-timer.232ce2d5.js
@@ -0,0 +1,2 @@
+document.querySelector(".s").addEventListener("click",(function(t){console.log((async()=>{let t=Date.now();try{await fetch("https://172.24.22.1")}catch(t){}return Date.now()-t})())}));
+//# sourceMappingURL=02-timer.232ce2d5.js.map
--- 02-timer.3cb5654e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".s").addEventListener("click",(function(t){console.log((async()=>{let t=Date.now();try{await fetch("http://172.24.22.1")}catch(t){}return Date.now()-t})())}));
-//# sourceMappingURL=02-timer.3cb5654e.js.map
